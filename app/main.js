e7bbcd9a38aa7255e22f18d5b6acad28cdfc04fd
romelcumare <romel.cumare@solidsolutions.co.uk>
2021-11-03 12:29:25 +0000

    updated electron

--- app/main.js
+++ app/main.js
@@ -2,10 +2,10 @@ const { app, BrowserWindow, ipcMain, dialog } = require('electron');
 const path = require( 'path' );
 const { autoUpdater } = require('electron-updater');
 
+console.log(ipcMain)
+
 // local dependencies
 const io = require( './main/io' );
-
-// let mainWindow;
 
 const openWindow = () => {
   const mainWindow = new BrowserWindow({
@@ -24,6 +24,8 @@ const openWindow = () => {
 // when app is ready, open a window
 app.on('ready', () => {
   const mainWindow = openWindow();
+  // mainWindow.webContents.openDevTools();
+  // Show dev tools
   autoUpdater.checkForUpdatesAndNotify();
 });
 
@@ -61,46 +63,46 @@ ipcMain.on('restart_app', () => {
 // From IO example
 /************************/
 
-// // return list of files
-// ipcMain.handle( 'app:get-files', () => {
-//   return io.getFiles();
-// } );
+// return list of files
+ipcMain.handle( 'app:get-files', () => {
+  return io.getFiles();
+} );
 
-// // listen to file(s) add event
-// ipcMain.handle( 'app:on-file-add', ( event, files = [] ) => {
-//   io.addFiles( files );
-// } );
+// listen to file(s) add event
+ipcMain.handle( 'app:on-file-add', ( event, files = [] ) => {
+  io.addFiles( files );
+} );
 
-// // open filesystem dialog to choose files
-// ipcMain.handle( 'app:on-fs-dialog-open', ( event ) => {
-//   const files = dialog.showOpenDialogSync( {
-//       properties: [ 'openFile', 'multiSelections' ],
-//   } );
+// open filesystem dialog to choose files
+ipcMain.handle( 'app:on-fs-dialog-open', ( event ) => {
+  const files = dialog.showOpenDialogSync( {
+      properties: [ 'openFile', 'multiSelections' ],
+  } );
 
-//   io.addFiles( files.map( filepath => {
-//       return {
-//           name: path.parse( filepath ).base,
-//           path: filepath,
-//       };
-//   } ) );
-// } );
+  io.addFiles( files.map( filepath => {
+      return {
+          name: path.parse( filepath ).base,
+          path: filepath,
+      };
+  } ) );
+} );
 
-// /*-----*/
+/*-----*/
 
-// // listen to file delete event
-// ipcMain.on( 'app:on-file-delete', ( event, file ) => {
-//   io.deleteFile( file.filepath );
-// } );
+// listen to file delete event
+ipcMain.on( 'app:on-file-delete', ( event, file ) => {
+  io.deleteFile( file.filepath );
+} );
 
-// // listen to file open event
-// ipcMain.on( 'app:on-file-open', ( event, file ) => {
-//   io.openFile( file.filepath );
-// } );
+// listen to file open event
+ipcMain.on( 'app:on-file-open', ( event, file ) => {
+  io.openFile( file.filepath );
+} );
 
-// // listen to file copy event
-// ipcMain.on( 'app:on-file-copy', ( event, file ) => {
-//   event.sender.startDrag( {
-//       file: file.filepath,
-//       icon: path.resolve( __dirname, './resources/paper.png' ),
-//   } );
-// } );
+// listen to file copy event
+ipcMain.on( 'app:on-file-copy', ( event, file ) => {
+  event.sender.startDrag( {
+      file: file.filepath,
+      icon: path.resolve( __dirname, './resources/paper.png' ),
+  } );
+} );
--- app/render/js/renderer.js
+++ app/render/js/renderer.js
@@ -1,5 +1,9 @@
 const dragDrop = require( 'drag-drop' );
 const { ipcRenderer } = require( 'electron' );
+const version = document.getElementById('version');
+const notification = document.getElementById('notification');
+const message = document.getElementById('message');
+const restartButton = document.getElementById('restart-button');
 
 // local dependencies
 const dom = require( './dom' );
@@ -45,4 +49,34 @@ window.openDialog = () => {
             dom.displayFiles( files );
         } );
     } );
+}
+
+
+// Updater
+
+ipcRenderer.send('app_version');
+ipcRenderer.on('app_version', (event, arg) => {
+  ipcRenderer.removeAllListeners('app_version');
+  version.innerText = 'Version ' + arg.version;
+});
+
+ipcRenderer.on('update_available', () => {
+  ipcRenderer.removeAllListeners('update_available');
+  message.innerText = 'A new update is available. Downloading now...';
+  notification.classList.remove('hidden');
+});
+
+ipcRenderer.on('update_downloaded', () => {
+  ipcRenderer.removeAllListeners('update_downloaded');
+  message.innerText = 'Update Downloaded. It will be installed on restart. Restart now?';
+  restartButton.classList.remove('hidden');
+  notification.classList.remove('hidden');
+});
+
+function closeNotification() {
+  notification.classList.add('hidden');
+}
+
+function restartApp() {
+  ipcRenderer.send('restart_app');
 }
